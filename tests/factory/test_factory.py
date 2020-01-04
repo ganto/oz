154648fddc17d1986781d3188a334cae9d1905a4
Reto Gantenbein <reto.gantenbein@linuxmonk.ch>
2020-01-04 23:35:58 +0100

    tests: Execute tests for Fedora 29/30

--- tests/factory/test_factory.py
+++ tests/factory/test_factory.py
@@ -130,7 +130,7 @@ def test_fedora_core():
 def test_fedora():
     for version in ["7", "8", "9", "10", "11", "12", "13", "14", "15", "16",
                     "17", "18", "19", "20", "21", "22", "23", "24", "25", "26",
-                    "27", "28"]:
+                    "27", "28", "29", "30"]:
         for arch in ["i386", "x86_64"]:
             for installtype in ["url", "iso"]:
                 runtest(distro='Fedora', version=version, arch=arch,
